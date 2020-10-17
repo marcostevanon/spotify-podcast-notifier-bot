aa8205faae2e0ffd80802c7270163034ab4fbe34
Marco Stevanon <marco.stevanon@gmail.com>
2020-10-18 00:19:56 +0200

    fix: add express, heroku require express to works

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { config } from 'dotenv';
+import express from 'express';
 import { Scheduler } from './helpers/scheduler';
 import { BotApp } from './telegram/bot';
 
@@ -13,3 +14,9 @@ console.log('Bot started!');
 const scheduler = new Scheduler();
 scheduler.startPolling();
 console.log('Scheduler started!');
+
+// Need express to start listening, without heroku make app crash
+// https://stackoverflow.com/a/15693371/8622120
+const PORT = process.env.PORT;
+express()
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
