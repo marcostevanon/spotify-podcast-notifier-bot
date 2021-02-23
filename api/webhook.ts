faa96e237c428bad62ef32358885aae8861d3426
Marco Stevanon <marco.stevanon@gmail.com>
2021-02-23 23:15:33 +0100

    feat: console log webhook

--- api/webhook.ts
+++ api/webhook.ts
@@ -7,6 +7,7 @@ const bot = botApp.init();
 export default async (request: VercelRequest, response: VercelResponse) => {
   try {
     await bot.handleUpdate(request.body, response);
+    console.log('webhook payload', request.body);
     response.status(200).end();
   } catch (err) {
     console.log(err)
